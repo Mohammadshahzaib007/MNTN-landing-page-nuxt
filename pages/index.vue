6fc0383d17eabe67a443ae8b014a7991e4c7d12a
croudit <croudit@pop-os.localdomain>
2021-02-01 19:11:14 +0530

    third section has been completed

--- pages/index.vue
+++ pages/index.vue
@@ -48,7 +48,7 @@
             style="position: relative"
           >
             <p
-              style="font-size: 15rem; font-weight: 700; color: rgba(225, 225, 225, .1); position: absolute; top: 0; left: -136px font-weight: 700"
+              style="font-size: 15rem; font-weight: 700; color: rgba(225, 225, 225, .1); position: absolute; top: -8px; left: -136px; font-weight: 700"
             >
               01
             </p>
@@ -101,6 +101,149 @@
           <v-col cols="6" class="d-flex justify-end">
             <v-img
               :src="require('@/assets/images/01.png')"
+              max-height="45rem"
+              max-width="35.375rem"
+              contain
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+
+    <!-- Section Third -->
+    <section style="margin-top: 12.5rem">
+      <v-container style="max-width: 91.375rem">
+        <v-row>
+          <v-col cols="6" class="d-flex">
+            <v-img
+              :src="require('@/assets/images/02.png')"
+              max-height="45rem"
+              max-width="35.375rem"
+              contain
+            />
+          </v-col>
+          <v-col
+            cols="6"
+            class="d-flex align-center justify-end"
+            style="position: relative"
+          >
+            <p
+              style="font-size: 15rem; font-weight: 700; color: rgba(225, 225, 225, .1); position: absolute; top: -8px; left: -64px; font-weight: 700"
+            >
+              02
+            </p>
+            <v-sheet color="transparent" class="px-0 py-0" max-height="439px">
+              <v-sheet
+                color="transparent"
+                class="d-flex align-center mb-0 py-0"
+              >
+                <span class="line"></span>
+                <p
+                  class="py-0 my-0 text-uppercase"
+                  style="font-size: 1.125rem; color: #FBD784; line-height: 22.05px; font-weight: 600; display: inline-block"
+                >
+                  Hiking Essentials
+                </p>
+              </v-sheet>
+
+              <h1
+                class="text-capitalize py-0 my-0"
+                style="color: white; font-size: 4rem; max-width: 555px; font-weight: 600; "
+              >
+                Picking the right Hiking Gear!
+              </h1>
+              <p
+                class="mt-0 py-0"
+                style="font-size: 18px; color: white; max-width: 632px;  font-weight: 800"
+              >
+                The nice thing about beginning hiking is that you don’t really
+                need any special gear, you can probably get away with things you
+                already have. Let’s start with clothing. A typical mistake
+                hiking beginners make is wearing jeans and regular clothes,
+                which will get heavy and chafe wif they get sweaty or wet.
+              </p>
+              <p
+                class="mt-5"
+                style="font-size: 18px; color: #FBD784;  font-weight: 800"
+              >
+                read more
+                <v-icon
+                  color="white"
+                  size="24"
+                  class="ml-4"
+                  style="transform: rotate(270deg);"
+                  >$downarr</v-icon
+                >
+              </p>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+
+    <!-- Section Fourth -->
+    <section style="margin-top: 12.5rem">
+      <v-container style="max-width: 91.375rem">
+        <v-row>
+          <v-col
+            cols="6"
+            class="d-flex align-center"
+            style="position: relative"
+          >
+            <p
+              style="font-size: 15rem; font-weight: 700; color: rgba(225, 225, 225, .1); position: absolute; top: -8px; left: -136px; font-weight: 700"
+            >
+              03
+            </p>
+            <v-sheet color="transparent" class="px-0 py-0" max-height="439px">
+              <v-sheet
+                color="transparent"
+                class="d-flex align-center mb-0 py-0"
+              >
+                <span class="line"></span>
+                <p
+                  class="py-0 my-0 text-uppercase"
+                  style="font-size: 1.125rem; color: #FBD784; line-height: 22.05px; font-weight: 600; display: inline-block"
+                >
+                  where you go is the key
+                </p>
+              </v-sheet>
+
+              <h1
+                class="text-capitalize py-0 my-0"
+                style="color: white; font-size: 4rem; max-width: 555px; font-weight: 600; "
+              >
+                Understand Your Map & Timing
+              </h1>
+              <p
+                class="mt-0 py-0"
+                style="font-size: 18px; color: white; max-width: 632px;  font-weight: 800"
+              >
+                To start, print out the hiking guide and map. If it’s raining,
+                throw them in a Zip-Lock bag. Read over the guide, study the
+                map, and have a good idea of what to expect. I like to know what
+                my next landmark is as I hike. For example, I’ll read the guide
+                and know that say, in a mile, I make a right turn at the
+                junction..
+              </p>
+              <p
+                class="mt-5"
+                style="font-size: 18px; color: #FBD784;  font-weight: 800"
+              >
+                read more
+                <v-icon
+                  color="white"
+                  size="24"
+                  class="ml-4"
+                  style="transform: rotate(270deg);"
+                  >$downarr</v-icon
+                >
+              </p>
+            </v-sheet>
+          </v-col>
+          <v-col cols="6" class="d-flex justify-end">
+            <v-img
+              :src="require('@/assets/images/03.png')"
               max-height="45rem"
               max-width="35.375rem"
               contain
