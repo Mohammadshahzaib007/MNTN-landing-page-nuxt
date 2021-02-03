881930c0b67a34b6a6291ffade81a2f3aebcf69d
Mohammad Shahzaib <mohdshahzaib22@gmail.com>
2021-02-03 20:07:03 +0530

    animations was added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <!-- Section First -->
-    <section>
+    <section id="hero-section">
       <v-container>
         <v-row>
           <v-col
@@ -9,7 +9,7 @@
             style="margin-top: 253px"
           >
             <v-sheet color="transparent">
-              <v-sheet color="transparent" class="d-flex align-center mb-8">
+              <v-sheet color="transparent" class="sub-super-text d-flex align-center mb-8">
                 <span class="line"></span>
                 <p
                   class="py-0 my-0 text-uppercase"
@@ -22,16 +22,40 @@
               <h1
                 class="text-capitalize text-h1"
                 style="color: white; max-width: 59.6875rem; font-weight: 600; "
+                ref="heading"
               >
-                Be prepared for the Mountains and beyond!
+                <v-sheet
+                  color="transparent"
+                  style="overflow: hidden; color: white;"
+                  class="px-0 py-0"
+                >
+                  <!-- <span ref="heading" class="d-block main-heading-animation"> -->
+                  Be prepared for the
+                  <!-- </span> -->
+                </v-sheet>
+                <v-sheet
+                  color="transparent"
+                  style="overflow: hidden; color: white;"
+                  class="px-0 py-0"
+                >
+                  Mountains and beyond!
+                </v-sheet>
               </h1>
-              <p
-                class="mt-5"
-                style="font-size: 18px; color: white;  font-weight: 800"
+              <v-sheet
+                color="transparent"
+                class="mt-5 scroll-down-text"
+                style="backface-visibility: hidden; font-size: 18px; color: white; font-weight: 800"
               >
-                scroll down
-                <v-icon color="white" size="24" class="ml-4 icon-animation">$downarr</v-icon>
-              </p>
+                <span 
+                  class="g-link"
+                  @click="$vuetify.goTo('#get-started-section', { duration: 1000 })"
+                >
+                  scroll down
+                </span>
+                <v-icon color="white" size="24" class="ml-1 icon-animation">
+                  $downarr
+                </v-icon>
+              </v-sheet>
             </v-sheet>
           </v-col>
         </v-row>
@@ -39,7 +63,7 @@
     </section>
 
     <!-- Section Second -->
-    <section style="margin-top: 28.9375rem;">
+    <section id="get-started-section" style="margin-top: 28.9375rem;">
       <v-container style="max-width: 91.375rem">
         <v-row>
           <v-col
@@ -67,10 +91,28 @@
               </v-sheet>
 
               <h2
+                v-observe-visibility="
+                  (isVisible, entry) =>
+                    visibilityChanged(isVisible, entry, 'section-2-heading')
+                "
+                ref="sectionSecondHeading"
                 class="py-0 my-0"
                 style="color: white; font-size: 4rem; max-width: 555px; font-weight: 600; line-height: 76.8px"
               >
-                What level of hiker are you?
+                <v-sheet
+                  color="transparent"
+                  style="overflow: hidden; color: white;"
+                  class="px-0 py-0"
+                >
+                  What level of hiker
+                </v-sheet>
+                <v-sheet
+                  color="transparent"
+                  style="overflow: hidden; color: white;"
+                  class="px-0 py-0"
+                >
+                  are you?
+                </v-sheet>
               </h2>
               <p
                 class="mt-3 py-0"
@@ -100,6 +142,7 @@
           </v-col>
           <v-col cols="6" class="d-flex justify-end">
             <v-img
+              v-tilt
               :src="require('@/assets/images/01.png')"
               max-height="45rem"
               max-width="35.375rem"
@@ -111,11 +154,12 @@
     </section>
 
     <!-- Section Third -->
-    <section style="margin-top: 12.5rem">
+    <section id="hiking-esentials-section" style="margin-top: 12.5rem">
       <v-container style="max-width: 91.375rem">
         <v-row>
           <v-col cols="6" class="d-flex">
             <v-img
+              v-tilt
               :src="require('@/assets/images/02.png')"
               max-height="45rem"
               max-width="35.375rem"
@@ -146,11 +190,29 @@
                 </p>
               </v-sheet>
 
+              <!-- v-observe-visibility="visibilityChanged" -->
               <h2
+                v-observe-visibility="
+                  (isVisible, entry) =>
+                    visibilityChanged(isVisible, entry, 'section-3-heading')
+                "
                 class="py-0 my-0"
+                ref="sectionThirdHeading"
                 style="color: white; font-size: 4rem; max-width: 555px; font-weight: 600; line-height: 76.8px"
               >
-                Picking the right Hiking Gear!
+                <v-sheet
+                  color="transparent"
+                  style="overflow: hidden; color: white;"
+                  class="px-0 py-0"
+                  >Picking the right
+                </v-sheet>
+                <v-sheet
+                  color="transparent"
+                  style="overflow: hidden; color: white;"
+                  class="px-0 py-0"
+                >
+                  Hiking Gear!
+                </v-sheet>
               </h2>
               <p
                 class="mt-3 py-0"
@@ -172,8 +234,9 @@
                   size="24"
                   class="ml-4"
                   style="transform: rotate(270deg);"
-                  >$downarr</v-icon
                 >
+                  $downarr
+                </v-icon>
               </p>
             </v-sheet>
           </v-col>
@@ -182,7 +245,10 @@
     </section>
 
     <!-- Section Fourth -->
-    <section style="margin-top: 12.5rem; margin-bottom: 150px">
+    <section
+      id="where-you-go-section"
+      style="margin-top: 12.5rem; margin-bottom: 150px"
+    >
       <v-container style="max-width: 91.375rem">
         <v-row>
           <v-col
@@ -209,11 +275,30 @@
                 </p>
               </v-sheet>
 
+              <!-- v-observe-visibility="visibilityChanged" -->
               <h2
+                v-observe-visibility="
+                  (isVisible, entry) =>
+                    visibilityChanged(isVisible, entry, 'section-4-heading')
+                "
+                ref="sectionFourthHeading"
                 class="py-0 my-0"
                 style="color: white; font-size: 4rem; max-width: 540px; font-weight: 600; line-height: 76.8px"
               >
-                Understand Your Map & Timing
+                <v-sheet
+                  color="transparent"
+                  style="overflow: hidden; color: white;"
+                  class="px-0 py-0"
+                >
+                  Understand Your
+                </v-sheet>
+                <v-sheet
+                  color="transparent"
+                  style="overflow: hidden; color: white;"
+                  class="px-0 py-0"
+                >
+                  Map & Timing
+                </v-sheet>
               </h2>
               <p
                 class="mt-3 py-0"
@@ -243,6 +328,7 @@
           </v-col>
           <v-col cols="6" class="d-flex justify-end">
             <v-img
+              v-tilt
               :src="require('@/assets/images/03.png')"
               max-height="45rem"
               max-width="35.375rem"
@@ -256,21 +342,105 @@
 </template>
 
 <script>
-export default {};
-</script>
+import { Component, Vue, Ref, Watch } from "nuxt-property-decorator";
+import { gsap } from "gsap";
 
+import "splitting/dist/splitting.css";
+import "splitting/dist/splitting-cells.css";
+import splitting from "splitting";
+
+@Component
+export default class Index extends Vue {
+  @Ref("heading") heading;
+  @Ref("sectionSecondHeading") sectionSecondHeading;
+  @Ref("sectionThirdHeading") sectionThirdHeading;
+  @Ref("sectionFourthHeading") sectionFourthHeading;
+
+  isSection2Visible = false;
+  tl2 = null;
+  tl3 = null;
+  tl4 = null;
+
+  visibilityChanged(isVisible, _entry, whichSection) {
+    this.isSection2Visible = isVisible;
+
+    // Checking we are on which section currently
+    // and then executing the code according to the section
+    if (whichSection === "section-2-heading") {
+      if (isVisible && this.tl2) {
+        this.tl2.play();
+      } else {
+        this.tl2.reverse();
+      }
+    }
+
+    if (whichSection === "section-3-heading") {
+      if (isVisible && this.tl3) {
+        this.tl3.play();
+      } else {
+        this.tl3.reverse();
+      }
+    }
+
+    if (whichSection === "section-4-heading") {
+      if (isVisible && this.tl4) {
+        this.tl4.play();
+      } else {
+        this.tl4.reverse();
+      }
+    }
+  }
+
+  mounted() {
+    //Splitting the heading into spans
+    splitting({ target: this.heading, by: "words" });
+    splitting({ target: this.sectionSecondHeading, by: "words" });
+    splitting({ target: this.sectionThirdHeading, by: "words" });
+    splitting({ target: this.sectionFourthHeading, by: "words" });
+    // gsap initializations
+    const tl = gsap.timeline({ paused: true });
+    this.tl2 = gsap.timeline({ paused: true });
+    this.tl3 = gsap.timeline({ paused: true });
+    this.tl4 = gsap.timeline({ paused: true });
+
+    // common animation
+    const textAnimation = {
+      opacity: 0,
+      y: 100,
+      duration: 0.4,
+      stagger: 0.11
+    };
+
+    // hero banner
+    tl.from("#hero-section .word", textAnimation)
+    .from('#navbar', { opacity: 0, duration: .2 })
+    .from('#hero-section .sub-super-text', { opacity: 0, duration: .2 }, '-=.2')
+    .from('#hero-section .scroll-down-text', { opacity: 0}, '-=.2')
+
+    // Section 2;
+    this.tl2.from("#get-started-section .word", textAnimation);
+
+    // Section 3;
+    this.tl3.from("#hiking-esentials-section .word", textAnimation);
+
+    // Section 4;
+    this.tl4.from("#where-you-go-section .word", textAnimation);
+
+    // On window load play the banner textAnimation
+    window.addEventListener("load", () => {
+      setTimeout(() => {
+        tl.play();
+      }, 500);
+    });
+  }
+}
+</script>
 <style lang="scss">
 #bg {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
-  // background-image: radial-gradient(
-  //     circle,
-  //     rgba(11, 29, 38, 0.2),
-  //     rgba(11, 29, 38, 1) 90%
-  //   ),
-  //   url("../assets/images/bg.jpg");
   background-image: linear-gradient(
       180deg,
       rgba(2, 0, 36, 0) -50%,
@@ -283,6 +453,7 @@ export default {};
   background-size: cover;
   background-position: center;
 }
+
 .line {
   width: 72px;
   height: 2px;
@@ -292,15 +463,22 @@ export default {};
 }
 
 .icon-animation {
-  animation: downAnimation 1s ease-in-out infinite;
+  animation: downAnimation 1.5s ease-in-out infinite;
+}
+
+.main-heading-animation {
+  color: white;
 }
 
 @keyframes downAnimation {
-  from {
+  0% {
     transform: translateY(0);
   }
-  to {
+  50% {
     transform: translateY(10px);
+  }
+  100%{
+    transform: translateY(0)
   }
 }
 </style>
